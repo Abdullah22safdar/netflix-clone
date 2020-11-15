2dda4b01339f1c29416c81680c6ca791a7070a5d
Abdullah22safdar <48974889+Abdullah22safdar@users.noreply.github.com>
2020-11-15 11:49:33 -0800

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 
 
 const config = {
-    apiKey: "AIzaSyDhq1fwHJcWtXZWebkrV8_cw7hLwlXbwlQ",
-    authDomain: "netflix-github.firebaseapp.com",
-    databaseURL: "https://netflix-github.firebaseio.com",
-    projectId: "netflix-github",
-    storageBucket: "netflix-github.appspot.com",
-    messagingSenderId: "482235519669",
-    appId: "1:482235519669:web:940e9dcdd96c4319c364fc",
-    measurementId: "G-2CT0FVFJBJ"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
